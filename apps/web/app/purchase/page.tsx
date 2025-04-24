b916111ce314e331d77a548b016cc941501dabd2
GIT-Marquestra <vabhi.verma2678@gmail.com>
2025-04-24 18:24:06 +0530

    done with adding connect button and starting with solidity smart contract logic

---
+++ apps/web/app/purchase/page.tsx
@@ -0,0 +1,11 @@
+import { ConnectButton } from '@repo/web3'
+import React from 'react'
+const Page = () => {
+  return (
+    <div>
+        <ConnectButton/>
+    </div>
+  )
+}
+
+export default Page
---
+++ packages/web3/src/providers/Web3Provider.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  ConnectButton
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+
+const config = getDefaultConfig({
+  appName: 'Mintverse',
+  projectId: 'YOUR_WALLETCONNECT_PROJECT_ID', 
+  chains: [sepolia],
+  ssr: true, 
+});
+
+const queryClient = new QueryClient();
+
+export function Web3Provider({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export { ConnectButton };
